d04cc78dd558abe2436fdf700f2c677a479f4d1a
Analiza <63755393+iamanaliza@users.noreply.github.com>
2020-09-21 15:59:08 -0500

    leaflet map

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
-const API_KEY = "pk.eyJ1IjoidmVlbmFjdiIsImEiOiJjazR6eGJ5ZWIwZjdmM210ZTgya3lwNzJiIn0.0qK5yFQHuncyG9NxIaUKQw";
-const MAPBOX_URL = "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}";
-const ATTRIBUTION = "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>";
+
+// API key
+const API_KEY = "pk.eyJ1IjoiYW1zYWF2ZWRyYTg1IiwiYSI6ImNrZXZuNXZ5ejBkdTQycXA1c3JlZHY1ZTMifQ.M1usnl7bzjd6cmO-CZPoog";
